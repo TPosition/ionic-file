e6baa279cfe3a366d3634cfdef3d3369f2a7c383
TPosition <zetanalpha@gmail.com>
2021-08-19 13:14:31 +0800

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { File } from '@ionic-native/file/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
-
+  private promise: Promise<string>;
+  private stringToWrite: string;
+  private blob: Blob;
+  constructor(
+  private platform: Platform,
+  private file: File,
+  ) {
+  this.platform.ready().then(() => {
+  });
+  }
+  createFile() {
+    console.log("file created");
+  this.file.createFile(this.file.dataDirectory, 'filename', true);
+  }
+  async readFile() {
+  this.promise = this.file.readAsText(this.file.dataDirectory, 'filename');
+  await this.promise.then(value => {
+  console.log(value);
+  });
+  }
+  writeFile() {
+  this.stringToWrite = 'I learned this from Medium';
+  this.blob = new Blob([this.stringToWrite], { type: 'text/plain' });
+  this.file.writeFile(this.file.dataDirectory, 'filename', this.blob, {replace: true, append: false});
+  }
 }
